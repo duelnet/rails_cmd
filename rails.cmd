22e546781f7a628569079b027640737f38d1029f
duelnet <103869498+duelnet@users.noreply.github.com>
2022-04-18 17:04:33 +0900

    Create rails.cmd

---
+++ rails.cmd
@@ -0,0 +1,22 @@
+$ rails new 프로젝트명                                 #루비 온 레일즈 프로잭트 생성
+$ code 프로젝트명                                      #Visual Studio Code 실행
+
+ctrl + ~                                                         # VScode에서 터미널 열기
+
+$ rails server                                                           #서버 작동되는지 확인
+                    
+ctrl + c                                                           #서버 중지
+
+$ rails generate controller 폴더명 파일명                                 #controller 생성
+
+
+
+$ rake db:create
+
+$ rails db:migrate                                       #push that migration into database and make it live
+
+
+<%= link_to 'Text', folder_file_path, class:"navbar-brand" %>
+<a class="navbar-brand" href="<filder_file_path">Text</a>
+
+
