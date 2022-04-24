f5651decc0c04bf0c78108c780cb606e996c1f6d
duelnet <103869498+duelnet@users.noreply.github.com>
2022-04-25 07:30:40 +0900

    Update rails.cmd

--- rails.cmd
+++ rails.cmd
@@ -25,3 +25,45 @@ $ rails generate devise:install
 
 
 $ bundle install        #grabs all file in Gemfile and installs them
+
+-------------------------------------------------------------------------------------------------------------------------------------------------
+$ rails new 프로젝트명
+$ rails routes
+$ rails server
+--------------------------------------------------------------------------
+$ rails generate controller 폴더 파일
+--------------------------------------------------------------------------
+$ rails generate scaffold 테이블명 이름1:타입 이름2:타입 이름3:타임		...
+$ rails db:migrate
+
+
+
+--------------------------------------------------------------------------
+
+
+
+
+
+----------------------------------------------------------------------
+1 50
+devise
+
+
+gemfile
+gem 'devise', '~> 4.8', '>= 4.8.1'
+
+$ bundle install
+$ rails generate devise:install
+
+At this point, a number of instructions will appear in the console. Among these instructions,
+you'll need to set up the default URL options for the Devise mailer in each environment.
+Here is a possible configuration for config/environments/development.rb:
+
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+$ rails generate deivse:views
+
+$ rails generate devise user
+
+$ rails db:migrate
